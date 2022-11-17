6b5dfda2be11f455f6f2d234241b4b8c8b3e3ce1
maskerliu <lynx.chrisliu@gmail.com>
2022-11-17 17:42:26 +0800

    fix bug

--- src/main/repository/iot.device.repo.ts
+++ src/main/repository/iot.device.repo.ts
@@ -6,5 +6,22 @@ import BaseRepo from './base.repo'
 @Repository(USER_DATA_DIR + '/biz_storage', 'IOT.Devices', ['deviceId'])
 export default class IOTDeviceRepo extends BaseRepo<IOT.IOTDevice> {
 
-
+  public async update(item: IOT.IOTDevice) {
+    let resultId: string = null
+    try {
+      let getResult = await this.get('deviceId', item.deviceId, ['_rev'])
+      let result = null
+      if (getResult) {
+        item._rev = getResult._rev
+        result = await this.pouchdb.put(item)
+      } else {
+        result = await this.pouchdb.post(item)
+      }
+      if (result.ok) resultId = result.id
+    } catch (err) {
+      throw '更新失败' + err
+    } finally {
+      return resultId
+    }
+  }
 }
